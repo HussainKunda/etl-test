fd22bfcc26001718c7fe67dda2dea426d43c1b6f
HussainKunda <Hussainkunda18@gmail.com>
2024-08-26 18:54:35 +0530

    test

--- long_running_script.py
+++ long_running_script.py
@@ -2,9 +2,9 @@ import time
 
 def main():
     print("Starting long-running task...")
-    for i in range (60):
+    for i in range (10):
         print(f"Minute {i+1}")
-        time.sleep(60)
+        time.sleep(10)
     print("Task completed")
 
 if __name__ == "__main__":
